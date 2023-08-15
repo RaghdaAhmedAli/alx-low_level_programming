98ffa7f866ace555cbf08156f1dd6ae0ae208cc1
RaghdaAhmedAli <raghda_ahmed@hotmail.com>
2023-08-15 05:36:15 -0700

    Size

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main -A program that print aline using printf function
+ * main - A program that using printf function
  * return: 0 (success)
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	retun(0);
 }
